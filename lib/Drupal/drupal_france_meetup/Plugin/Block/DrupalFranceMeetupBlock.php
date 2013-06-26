bd33bef8e5206dd56e8ae202bf5959fd2fe85446
Cyril Gravelier <magiccyril@gmail.com>
2013-06-26 23:49:04 +0200

    Get the feed and call the theme

--- lib/Drupal/drupal_france_meetup/Plugin/Block/DrupalFranceMeetupBlock.php
+++ lib/Drupal/drupal_france_meetup/Plugin/Block/DrupalFranceMeetupBlock.php
@@ -10,6 +10,7 @@ namespace Drupal\drupal_france_meetup\Plugin\Block;
 use Drupal\block\BlockBase;
 use Drupal\Component\Annotation\Plugin;
 use Drupal\Core\Annotation\Translation;
+use Zend\Feed\Reader\Reader;
 
 /**
  * Provides a simple block with future Drupal France meetups.
@@ -23,19 +24,59 @@ use Drupal\Core\Annotation\Translation;
 class DrupalFranceMeetupBlock extends BlockBase {
 
   /**
-   * Get future Drupal France Meetups
-   * @return Array [description]
+   * Get XML String from URL
+   *
+   * @return String XML string
    */
-  protected function getFutureMeetups() {
+  protected function getXmlString($url) {
+    $request = \Drupal::httpClient()->get($url);
 
+    try {
+      $response = $request->send();
+      $xml = $response->getBody()->__toString();
+    }
+    catch (\Exception $e) {
+      drupal_set_message(t('Cannot access RSS feed.'), 'error');
+
+      return '';
+    }
+
+    return $xml;
   }
 
   /**
-   * Implements \Drupal\block\BlockBase::blockBuild().
+   * Get future Drupal France Meetups
+   *
+   * @return Array Array of entries
    */
-  protected function blockBuild() {
+  protected function getFutureMeetups() {
+    $entries = array();
+
+    $site_config = config('drupal_france_meetup.settings');
+    $rss = $site_config->get('rss');
+    $nb_items = $site_config->get('nb_items');
+
+    if ($rss) {
+      $xml_string = $this->getXmlString($rss);
+      $channel = Reader::importString($xml_string);
+
+      foreach ($channel as $item) {
+        if (count($entries) < $nb_items) {
+          array_push($entries, l($item->getTitle(), $item->getId()));
+        }
+      }
+    }
+
+    return $entries;
+  }
+
+  /**
+   * Implements \Drupal\block\BlockBase::build().
+   */
+  public function build() {
     return array(
-      '#children' => 'This is a block!',
+      '#entries' => $this->getFutureMeetups(),
+      '#theme'   => 'drupal_france_meetup_block',
     );
   }
 
